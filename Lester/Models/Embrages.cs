5c1583af9e56e9ee3dfa1b357c4507c2815a031e
¨israel643¨ <utp0140477@alumno.utpuebla.edu.mx>
2024-05-30 09:07:01 -0600

    Agregar archivos de proyecto.

---
+++ Lester/Models/Embrages.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lester.Models
+{
+    public class Embrages
+    {
+        public int Id { get; set; }
+        public string codebar { get; set; }
+        public string acronimo { get; set; }
+        public DateTime fechaLectura { get; set; }
+        public string objReferencia { get; set; }
+        public int tipo {  get; set; }
+        public string Viaje { get; set; }
+
+    }
+}
