0c4b294759f26b36cd60dbf7faf53c8236cec611
¨israel643¨ <utp0140477@alumno.utpuebla.edu.mx>
2024-05-30 12:31:11 -0600

    Conexión con SQL client

--- Lester/Controllers/HomeController.cs
+++ Lester/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Lester.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,13 +11,18 @@ namespace Lester.Controllers
     {
         public ActionResult Index()
         {
+            DataAccessDAL dataAccess = new DataAccessDAL();
+            bool conexionExitosa = dataAccess.ProbarConexion();
+            ViewBag.ConexionExitosa = conexionExitosa;
+
             return View();
         }
 
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
+            
+           
             return View();
         }
 
---
+++ Lester/Models/DataAccessDAL.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient; /// Importación para trabajar con provedor de Datos 
+
+namespace Lester.Models
+{
+    public class DataAccessDAL
+    {
+        private string cadena;
+
+        private DataAccessDAL() 
+        {
+            cadena = System.Configuration.ConfigurationManager.ConnectionStrings["Lester"].ConnectionString; 
+        }
+
+        public bool ProbarConexion() // Función para probar la conexión atravez de una consulta
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(cadena))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand("SELECT COUNT (*) FROM tblRFID_CodiCaptEmbarques", connection))
+                    {
+                        int count = (int)command.ExecuteScalar();
+                        return true;
+                    }
+                }
+            }
+            catch (Exception ex) {
+                Console.WriteLine("Error:" + ex.Message);
+                return false;
+            }
+            }
+    }
+}
